d6e776b9e6f14e78c9564bbf1785a18aa9268f4c
kk727023 <102048309+kk727023@users.noreply.github.com>
2024-10-16 22:41:15 +0800

    新增HeaderComponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <HeaderComponent msg="Welcome to My Site.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeaderComponent
   }
 }
 </script>
