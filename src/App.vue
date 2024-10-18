eb5ee4da09a6adab56098774160be14e3e4f8c59
kk727023 <102048309+kk727023@users.noreply.github.com>
2024-10-18 21:53:07 +0800

    將大頭貼變成圓形並加入外框

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,10 @@ export default {
   margin-top: 60px;
 }
 #photo {
-  width: 10%;
-  height: 10%;
+  width: 300px;
+  height: 300px;
+  border-radius: 50%;
+  border: 10px solid #3498db; 
+  object-fit: cover; 
 }
 </style>
