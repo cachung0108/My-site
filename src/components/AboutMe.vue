515a9f81402eae829b8bbe0c3a873c5f9a048f21
kk727023 <102048309+kk727023@users.noreply.github.com>
2024-10-23 00:34:56 +0800

    新增文字區塊

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,16 +1,18 @@
 <template>
     <div class = "aboutMe">
-        <p>
-            我的名字叫鍾期安，目前在中興大學無線網路實驗室進行網路相關研究。​在大學時期曾經完成許多專案包括：
-            <br>
-            網頁程式設計課使用HTML、JS、CSS完成嘉義番路鄉的介紹網站​
-            <br>
-            畢業專題負責網頁程式的前端以及資料庫，使用Vue.js以及MongoDB完成​
-            <br>
-            目前於中興大學無線通訊暨雲端計算進行802.11be MLO技術研究。​
-            <br>
-            碩一時修習了新型態網路技術、雲端計算與網路課程，除此之外也擔任實驗室研究助理。​
-        </p>    
+        <div class="textBlock">
+            <p>
+                我的名字叫鍾期安，目前在中興大學無線網路實驗室進行網路相關研究。​在大學時期曾經完成許多專案包括：
+                <br>
+                網頁程式設計課使用HTML、JS、CSS完成嘉義番路鄉的介紹網站​
+                <br>
+                畢業專題負責網頁程式的前端以及資料庫，使用Vue.js以及MongoDB完成​
+                <br>
+                目前於中興大學無線通訊暨雲端計算進行802.11be MLO技術研究。​
+                <br>
+                碩一時修習了新型態網路技術、雲端計算與網路課程，除此之外也擔任實驗室研究助理。​
+            </p>    
+        </div>
     </div>
 </template>
 
@@ -22,5 +24,13 @@
     display: flex;
     justify-content: center; /* 讓內容在水平居中 */
     align-items: center;     /* 讓內容在垂直居中 */
+}
+.textBlock {
+    background-color: #f9f9f9; /* 文字區塊背景設置為淺灰色 */
+    padding: 15px; /* 內部邊距 */
+    margin-bottom: 15px; /* 區塊間距 */
+    border: 1px solid #ddd; /* 添加邊框 */
+    border-radius: 5px; /* 邊角圓滑 */
+    text-align: left; /* 文字靠左對齊 */
 }
 </style>
