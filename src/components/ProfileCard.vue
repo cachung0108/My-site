8f87eeb83ad0030526cc6b89d47b82adb81d1a2e
kk727023 <102048309+kk727023@users.noreply.github.com>
2024-10-25 23:56:40 +0800

    將相關連結改成圖片按鈕

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,14 +1,22 @@
 <template>
     <div class="profileCard">
-        <img alt="Photo stickers" src="../assets/photo.jpg" id="photo">
+        <img alt="Photo stickers" src="../assets/images/photo.jpg" id="photo">
         <header>
             <h1>Chi-An Chung</h1>
             <p>NCHU CSIE</p>
-            <a href="mailto:cachung0108@gmail.com">cachung0108@gmail.com</a>
-            <br>
-            <a href="https://github.com/cachung0108" target="_blank">GitHub</a>
-            <br>
-            <a href="https://www.linkedin.com/in/chi-an-chung-63bb17316/" target="_blank">LinkedIn</a>
+            <div class="icon-container">
+                <a href="mailto:cachung0108@gmail.com">
+                    <img src="../assets/images/gmail.png" alt="點擊圖片跳轉到網站" class="clickable-image">
+                </a>
+                <br>
+                <a href="https://github.com/cachung0108" target="_blank">
+                    <img src="../assets/images/github.png" alt="點擊圖片跳轉到網站" class="clickable-image">
+                </a>
+                <br>
+                <a href="https://www.linkedin.com/in/chi-an-chung-63bb17316/" target="_blank">
+                    <img src="../assets/images/linkedin.png" alt="點擊圖片跳轉到網站" class="clickable-image">
+                </a>
+            </div>
         </header>
     </div>
 </template>
@@ -25,5 +33,22 @@
   border-radius: 50%;
   border: 10px solid #3498db; 
   object-fit: cover; 
+}
+.clickable-image {
+  width: 100px;
+  height: 100px;
+  border-radius: 8px;        /* 圓角 */
+  box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2); /* 陰影 */
+  transition: transform 0.2s; /* 點擊時縮放效果 */
+}
+
+.clickable-image:hover {
+  transform: scale(1.05); /* 鼠標懸停效果 */
+}
+.icon-container {
+    display: flex;
+    justify-content: center; /* 水平居中 */
+    gap: 10px; /* 圖片之間的間距 */
+    margin-top: 10px;
 }
 </style>
